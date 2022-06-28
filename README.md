980f3af57bad464848444d85fc11b70b7dc42ac8
2011 Alexandre <2011brunofernandes@gmail.com>
2022-06-27 21:34:44 -0300

    'update'

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # DevsbookOO
 Sistemas de rede social em PHP
+parei aqui
+https://alunos.b7web.com.br/curso/php/pagina-de-cadastro-3-oo
---
+++ login_action.php
@@ -0,0 +1,17 @@
+<?php 
+require 'config.php';
+require 'models/Auth.php';
+
+$email = filter_input(INPUT_POST, 'email');
+$password = filter_input(INPUT_POST, 'password');
+
+if($email && $password){
+    $auth = new Auth($pdo, $base);
+    if($auth->validateLogin($email, $password)){
+        header("location:".$base);
+        exit;
+    }
+}
+$_SESSION['flash'] = 'email ou senha incorretos';
+header ('location: '.$base.'/login.php');
+exit;
