4de204042508b6f98acca60e97eb82188c2b5477
2011 Alexandre <2011brunofernandes@gmail.com>
2022-07-12 15:38:47 -0300

    cadastro v2

--- signup_action.php
+++ signup_action.php
@@ -10,6 +10,33 @@ $birthdate = filter_input(INPUT_POST, 'birthdate');
 
 if($name && $email && $password && $password){
     $auth = new Auth($pdo, $base);
+
+    $birthdate = explode('/',$birthdate);
+    if(count($birthdate)!=3){
+        $_SESSION['flash'] = "Data de nascimento invalida";
+        header("location: ".$base."/login.php");
+        exit;
+    }
+
+    $birthdate = $birthdate[2].'-'.$birthdate[1].'-'.$birthdate[0];
+    if(strtotime($birthdate) === false){
+        $_SESSION['flash'] = "Data de nascimento invalida";
+        header("location: ".$base."/login.php");
+        exit;
+    }
+    
+    // verifica se email existe
+    if($auth->emailExists($email) === false){
+        $auth->registerUser($name, $email, $password, $birthdate);
+        header("location: ".$base);
+        exit;
+
+    }else{
+        $_SESSION['flash'] = "Email ja cadastrado";
+        header("location: ".$base."/login.php");
+        exit;
+    }
+
 }   
     
 $_SESSION['flash'] = "Campos nao enviados";
