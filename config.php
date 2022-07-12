79b01f19ba8a14dcbbad514319f923ec46bb8ed7
2011 Alexandre <2011brunofernandes@gmail.com>
2022-07-12 09:53:25 -0300

    add conexao

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php 
+
+$base = "http://localhost:8080/PHP/cursos/b7web/DevsbookOO/";
+
+$db_name = "devsbook";
+$db_host='localhost';
+$db_user = 'root';
+$db_pass = '';
+
+$pdo = new PDO("mysql:dbname=$db_name;host=$db_host",$db_user,$db_pass);
