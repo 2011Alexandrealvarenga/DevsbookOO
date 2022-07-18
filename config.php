4acf10e66ca90cb857619dceeee293475420bca1
2011 Alexandre <2011brunofernandes@gmail.com>
2022-07-18 19:17:11 -0300

    login - verificacao se tem tokien

--- config.php
+++ config.php
@@ -1,1 +1,11 @@
 
+<?php 
+session_start();
+$base = "http://localhost:8080/PHP/cursos/b7web/projetos/DevsbookOO";
+
+$db_name = "devsbook";
+$db_host='localhost';
+$db_user = 'root';
+$db_pass = '';
+
+$pdo = new PDO("mysql:dbname=$db_name;host=$db_host",$db_user,$db_pass);
