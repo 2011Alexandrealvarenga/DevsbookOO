bb243017ebe1d1a52c6e874d9a5b7cbfee4354cb
2011 Alexandre <2011brunofernandes@gmail.com>
2022-07-19 15:00:24 -0300

    home - pegar o relacionamento entre usuarios, usuarios que se seguem

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <?php 
 require 'config.php';
 require 'models/Auth.php';
+require 'dao/UserRelationPostDaoMysql.php';
 
 $auth = new Auth($pdo, $base);
 $userInfo = $auth->checkToken();
 $activeMenu = 'home';
+
+// 1 lista dos usuarios que eu sigo
+$urDao = new UserRelationDaoMysql($pdo);
+$userList = $urDao->getRelationsFrom($userInfo->id);
+
+// 2 pegar os posts ordenado pela data
+
+// 3 transforma o resultado em objetos
 
 require 'partials/header.php';
 require 'partials/menu.php';
@@ -15,66 +24,7 @@ require 'partials/menu.php';
         <div class="column pr-5">
 
             <?php require 'partials/feed-editor.php'; ?>
-
-            <div class="box feed-item">
-                <div class="box-body">
-                    <div class="feed-item-head row mt-20 m-width-20">
-                        <div class="feed-item-head-photo">
-                            <a href=""><img src="media/avatars/avatar.jpg" /></a>
-                        </div>
-                        <div class="feed-item-head-info">
-                            <a href=""><span class="fidi-name">Bonieky Lacerda</span></a>
-                            <span class="fidi-action">fez um post</span>
-                            <br/>
-                            <span class="fidi-date">07/03/2020</span>
-                        </div>
-                        <div class="feed-item-head-btn">
-                            <img src="assets/images/more.png" />
-                        </div>
-                    </div>
-                    <div class="feed-item-body mt-10 m-width-20">
-                        Pessoal, tudo bem! Busco parceiros para empreender comigo em meu software.<br/><br/>
-                        Acabei de aprová-lo na Appstore. É um sistema de atendimento via WhatsApp multi-atendentes para auxiliar empresas.<br/><br/>
-                        Este sistema permite que vários funcionários/colaboradores da empresa atendam um mesmo número de WhatsApp, mesmo que estejam trabalhando remotamente, sendo que cada um acessa com um login e senha particular....
-                    </div>
-                    <div class="feed-item-buttons row mt-20 m-width-20">
-                        <div class="like-btn on">56</div>
-                        <div class="msg-btn">3</div>
-                    </div>
-                    <div class="feed-item-comments">
-                        
-                        <div class="fic-item row m-height-10 m-width-20">
-                            <div class="fic-item-photo">
-                                <a href=""><img src="media/avatars/avatar.jpg" /></a>
-                            </div>
-                            <div class="fic-item-info">
-                                <a href="">Bonieky Lacerda</a>
-                                Comentando no meu próprio post
-                            </div>
-                        </div>
-
-                        <div class="fic-item row m-height-10 m-width-20">
-                            <div class="fic-item-photo">
-                                <a href=""><img src="media/avatars/avatar.jpg" /></a>
-                            </div>
-                            <div class="fic-item-info">
-                                <a href="">Bonieky Lacerda</a>
-                                Muito legal, parabéns!
-                            </div>
-                        </div>
-
-                        <div class="fic-answer row m-height-10 m-width-20">
-                            <div class="fic-item-photo">
-                                <a href=""><img src="media/avatars/avatar.jpg" /></a>
-                            </div>
-                            <input type="text" class="fic-item-field" placeholder="Escreva um comentário" />
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-
-            
+        
 
         </div>
         <div class="column side pl-5">
